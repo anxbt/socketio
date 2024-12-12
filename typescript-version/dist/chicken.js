1dd3ce6113435c50516ca1500b0f898bae957dd1
anxbt <anubrat23@gmail.com>
2024-12-12 23:08:18 +0530

    typescript version removed

--- typescript-version/dist/chicken.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Chicken = void 0;
-class Chicken {
-    cluck() {
-        console.log("cluck");
-    }
-}
-exports.Chicken = Chicken;
